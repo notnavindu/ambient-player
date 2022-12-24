d7e174088edff3e4858b2a1b3e25832d20cf96a9
Navindu <navanavi3@gmail.com>
2022-12-24 17:48:26 +0530

    feat: player

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	let paused = true;
+</script>
+
+<main class="h-screen w-full bg-black flex items-center justify-center p-5">
+	<div class=" fixed blur-3xl contrast-150 opacity-50">
+		<video autoplay loop class="w-full max-w-full m-auto" bind:paused muted>
+			<source src="ambient_sample.mp4" type="video/mp4" />
+			<track kind="captions" />
+		</video>
+	</div>
+
+	<div class="w-full max-w-4xl rounded-lg overflow-hidden z-50">
+		<video autoplay loop bind:paused muted class="shadow-2xl">
+			<source src="ambient_sample.mp4" type="video/mp4" />
+			<track kind="captions" />
+		</video>
+	</div>
+</main>
